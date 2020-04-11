1f19cd4e48ad10095a9be7beb59661dc57c48b86
incognico <nico@lifeisabug.com>
2020-04-11 15:46:34 +0200

    Add button to copy console connect command

--- www/htdocs/js/main.js
+++ www/htdocs/js/main.js
@@ -8,3 +8,16 @@ $("button").click((event)=>{
     $('tr[data-id="'+id+'_details"]').toggle();
   }
 });
+
+clipboard.on('success', function(e) {
+  var ele = e.trigger;
+  e.clearSelection();
+  clearTimeout();
+  ele.innerHTML = '&#x2714;';
+  ele.blur();
+  setTimeout(revert, 5000, ele);
+});
+
+function revert(ele) {
+  ele.innerHTML = '&#x1f517;';
+}
